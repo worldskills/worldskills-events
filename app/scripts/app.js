3cc78432065b51afa8996ce666741a76841b1be3
Fabian Vogler <fabian@equivalence.ch>
2014-02-20 13:25:52 +0100

    Added EventDetailCtrl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,6 +10,10 @@
         $routeProvider.when('/events', {
             templateUrl: 'views/events.html',
             controller: 'EventsCtrl'
+        }).when('/events/:id', {
+            templateUrl: 'views/event-detail.html',
+            controller: 'EventDetailCtrl',
+            requireLoggedIn: true
         }).otherwise({
             redirectTo: '/events'
         });
--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -4,4 +4,10 @@
     angular.module('eventsApp').controller('EventsCtrl', function($scope, Event) {
         $scope.events = Event.query();
     });
+
+    angular.module('eventsApp').controller('EventDetailCtrl', function($scope, $routeParams, Event) {
+        $scope.event = Event.get({id: $routeParams.id}, function (event) {
+            $scope.title = event.name;
+        });
+    });
 })();
