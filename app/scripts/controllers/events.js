6daf506c76bf67d66dbb93a5522bbc6eec383047
Fabian Vogler <fabian@equivalence.ch>
2014-02-21 10:27:31 +0100

    Sort countries

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -5,13 +5,17 @@
         $scope.events = Event.query();
     });
 
-    angular.module('eventsApp').controller('EventDetailCtrl', function($scope, $routeParams, Event, $http, API_EVENTS) {
+    angular.module('eventsApp').controller('EventDetailCtrl', function($scope, $routeParams, Event, $http, API_EVENTS, $translate) {
         $scope.event = Event.get({id: $routeParams.id}, function (event) {
             $scope.title = event.name;
         });
         $http({method: 'GET', url: API_EVENTS + '/countries'}).success(function(data, status, headers, config) {
-            console.log(data);
-            $scope.countries = data.countries;
+            $scope.countries = [];
+            angular.forEach(data.countries, function (code) {
+                $translate(code).then(function (name) {                    
+                    $scope.countries.push({code: code, name: name});
+                });
+            });
         });
     });
 })();
