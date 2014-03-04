eeca0d7096510964137d4648a261e0357794abac
Fabian Vogler <fabian@equivalence.ch>
2014-03-04 16:55:52 +0100

    Added sector and description to skill

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -1,11 +1,15 @@
 (function() {
     'use strict';
 
-    angular.module('eventsApp').controller('SkillCtrl', function($scope, $stateParams, Skill, $http, API_EVENTS, $translate, $state) {
+    angular.module('eventsApp').controller('SkillCtrl', function($scope, $stateParams, Skill, $http, API_EVENTS, $translate, $state, WorldSkills) {
         $scope.id = $stateParams.id;
         $scope.page = $stateParams.page;
         $scope.skill = Skill.get({id: $scope.id}, function (skill) {
             $scope.title = skill.name.text;
+            var url = WorldSkills.getLink(skill.event.links, 'sectors');
+            $http({method: 'GET', url: url}).success(function(data, status, headers, config) {
+                $scope.sectors = data.sectors;
+            });
         });
     });
 })();
