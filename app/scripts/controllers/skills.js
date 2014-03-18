baba0c767561f28f8f1de23e0aac80bae5047355
Fabian Vogler <fabian@equivalence.ch>
2014-03-18 13:29:04 +0100

    Added success message for Skill create and update

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -1,25 +1,24 @@
 (function() {
     'use strict';
 
-    angular.module('eventsApp').controller('SkillCreateCtrl', function($scope, $stateParams, Skill, Event, $http, API_EVENTS, $translate, $state, WorldSkills) {
+    angular.module('eventsApp').controller('SkillCreateCtrl', function($scope, $stateParams, Skill, Event, $http, API_EVENTS, $translate, $state, WorldSkills, alert) {
         $scope.skill = new Skill();
         $scope.skill.name = {text: '', lang_code: 'en'};
         $scope.skill.description = {text: '', lang_code: 'en'};
         $scope.skill.event = Event.get({id: $stateParams.eventId});
         $scope.save = function() {
             $scope.submitted = true;
-            if ($scope.form.$invalid) {
-                angular.element($scope.form).find('.ng-invalid').focus();
-                return;
+            if ($scope.form.$valid) {
+                $scope.loading = true;
+                $scope.skill.$save(function () {
+                    alert.success('The Skill has been added successfully.');
+                    $state.go('event.skills', {id: $scope.skill.event.id});
+                });
             }
-            $scope.loading = true;
-            $scope.skill.$save(function () {
-                $state.go('event.skills', {id: $scope.skill.event.id});
-            });
         };
     });
 
-    angular.module('eventsApp').controller('SkillCtrl', function($scope, $stateParams, Skill, $http, API_EVENTS, $translate, $state, WorldSkills) {
+    angular.module('eventsApp').controller('SkillCtrl', function($scope, $stateParams, Skill, $http, API_EVENTS, $translate, $state, WorldSkills, alert) {
         $scope.id = $stateParams.id;
         $scope.skill = Skill.get({id: $scope.id}, function (skill) {
             $scope.title = skill.name.text;
@@ -30,19 +29,18 @@
         });
         $scope.save = function() {
             $scope.submitted = true;
-            if ($scope.form.$invalid) {
-                angular.element($scope.form).find('.ng-invalid' ).focus();
-                return;
+            if ($scope.form.$valid) {
+                $scope.loading = true;
+                $scope.skill.$update(function () {
+                    alert.success('The Skill has been updated successfully.');
+                    $state.go('event.skills', {id: $scope.skill.event.id});
+                });
             }
-            $scope.loading = true;
-            $scope.skill.$update(function () {
-                $state.go('event.skills', {id: $scope.skill.event.id});
-            });
         };
         $scope.remove = function() {
             $scope.deleteLoading = true;
             $scope.skill.$delete(function () {
-                alert('The Skill has been deleted successfully.');
+                alert.success('The Skill has been deleted successfully.');
                 $state.go('event.skills', {id: $scope.skill.event.id});
             });
         };
