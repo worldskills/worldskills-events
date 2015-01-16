a0d27cab5cd7a614c2dbb3d70adbf96f9ad4d2b3
Fabian Vogler <fabian@equivalence.ch>
2015-01-16 14:23:07 +0100

    Convert event date to string

--- app/scripts/services/Event.js
+++ app/scripts/services/Event.js
@@ -1,15 +1,38 @@
 (function() {
     'use strict';
 
-    angular.module('eventsApp').service('Event', function($resource, WORLDSKILLS_API_EVENTS) {
+    angular.module('eventsApp').service('Event', function($resource, $filter, WORLDSKILLS_API_EVENTS) {
+
+        function convertDate(data) {
+            var event = data;
+            if (angular.isDate(event.start_date)) {
+                event.start_date = $filter('date')(event.start_date, 'yyyy-MM-dd');
+            }
+            if (angular.isDate(event.end_date)) {
+                event.end_date = $filter('date')(event.end_date, 'yyyy-MM-dd');
+            }
+            if (angular.isDate(event.competition_start_date)) {
+                event.competition_start_date = $filter('date')(event.competition_start_date, 'yyyy-MM-dd');
+            }
+            if (angular.isDate(event.competition_end_date)) {
+                event.competition_start_date = $filter('date')(event.competition_end_date, 'yyyy-MM-dd');
+            }
+            return angular.toJson(event);
+        }
+
         return $resource(WORLDSKILLS_API_EVENTS + '/:id', {
             id: '@id'
         }, {
             query: {
                 method: 'GET'
             },
+            save: {
+                method: 'POST',
+                transformRequest: convertDate
+            },
             update: {
-                method: 'PUT'
+                method: 'PUT',
+                transformRequest: convertDate
             }
         });
     });
