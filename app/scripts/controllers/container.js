f2da0cd181bc09edc0274a97373647c0902aa29a
Fabian Vogler <fabian@equivalence.ch>
2015-06-26 14:11:10 +0200

    Replace logout() with auth.logout()

--- app/scripts/controllers/container.js
+++ app/scripts/controllers/container.js
@@ -3,9 +3,6 @@
 
     angular.module('eventsApp').controller('ContainerCtrl', function($scope, auth, alert) {
         $scope.auth = auth;
-        $scope.logout = function (e) {
-            auth.logout();
-        };
         $scope.$on('$stateChangeStart', function () {
             alert.clear();
         });
