9dd5f112e3a9e7a1797db849626f8ce9b523da11
Fabian Vogler <fabian@equivalence.ch>
2015-11-04 12:23:40 +0100

    Add global event search

--- app/scripts/controllers/container.js
+++ app/scripts/controllers/container.js
@@ -1,11 +1,14 @@
 (function() {
     'use strict';
 
-    angular.module('eventsApp').controller('ContainerCtrl', function($scope, auth, alert) {
+    angular.module('eventsApp').controller('ContainerCtrl', function($scope, $state, auth, alert) {
         $scope.auth = auth;
         $scope.$on('$stateChangeStart', function () {
             alert.clear();
         });
         $scope.date = new Date();
+        $scope.search = function () {
+            $state.go('events.list', {name: $scope.searchText}, {reload: true});
+        };
     });
 })();
