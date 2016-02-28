b0a942be5b9751c4d5270c3f0f4be0504d5387a0
Fabian Vogler <fabian@equivalence.ch>
2016-02-28 21:24:56 +0100

    Add body-parser to mock API
    For JSON requests

--- test/apiMock.js
+++ test/apiMock.js
@@ -1,9 +1,11 @@
 
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   console.log("Mock server request: " + req.url);
