5c1d8f4d354082f8f9c35c357a92589f29a6a5d2
Fabian Vogler <fabian@equivalence.ch>
2016-03-03 10:04:54 +0100

    Add ngMessages

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function(config) {
             'app/bower_components/angular/angular.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
             'app/bower_components/angular-resource/angular-resource.js',
+            'app/bower_components/angular-messages/angular-messages.js',
             'app/bower_components/angular-sanitize/angular-sanitize.js',
             'app/bower_components/angular-translate/angular-translate.js',
             'app/bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
