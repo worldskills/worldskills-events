4c10d3627b0262898448348d69b3448d008f6d14
Fabian Vogler <fabian@equivalence.ch>
2016-05-03 21:08:30 +0200

    Remove cdnify from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,13 +181,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -272,7 +265,6 @@ module.exports = function (grunt) {
     'concat',
     'ngmin',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'rev',
