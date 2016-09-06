bbf9aef7e47aa450dd35e6b61b6d476326d09cf3
Fabian Vogler <fabian@equivalence.ch>
2016-09-06 09:20:06 +0200

    Add event.utc_offset

--- app/scripts/directives/form.js
+++ app/scripts/directives/form.js
@@ -11,4 +11,18 @@
             }
         };
     });
+
+    angular.module('eventsApp').directive('convertToNumber', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModel) {
+                ngModel.$parsers.push(function(val) {
+                    return parseInt(val, 10);
+                });
+                ngModel.$formatters.push(function(val) {
+                    return '' + val;
+                });
+            }
+        };
+    });
 })();
