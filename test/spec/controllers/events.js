850fd4e15b342805b3561934cbea4fda26e694fc
Fabian Vogler <fabian@equivalence.ch>
2019-06-20 15:44:31 +0200

    Fix tests

--- test/spec/controllers/events.js
+++ test/spec/controllers/events.js
@@ -48,7 +48,7 @@ describe('controllers events', function() {
                     }
                 ]
             });
-            $httpBackend.expectGET('http://localhost:8080/auth/ws_entities?limit=900').respond({
+            $httpBackend.expectGET('http://localhost:8080/events/ws_entities').respond({
                 entities: [
                     {
                         id: 1,
@@ -214,7 +214,7 @@ describe('controllers events', function() {
                     }
                 ]
             });
-            $httpBackend.expectGET('http://localhost:8080/auth/ws_entities?limit=900&role=EditEvents&roleApp=400').respond({
+            $httpBackend.expectGET('http://localhost:8080/auth/ws_entities?limit=9000&role=EditEvents&roleApp=400').respond({
                 entities: [
                     {
                         id: 1,
