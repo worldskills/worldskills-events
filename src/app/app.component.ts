9f604b646a3b2f6e581d03f74bc963f9c109746e
Waseem <waseemsabjee@gmail.com>
2025-01-20 08:49:53 +0200

    Cleanup app component modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
 import {
-  PuppeteerBannerComponent,
   BreadcrumbsService,
   Language,
   MenuItem,
